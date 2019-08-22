4ed1fefdad66e93a9bd2baf83a5f3b55760de2ad
Denali Balser <denali.balser167@gmail.com>
2019-08-22 14:02:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+
+  @@roster = {}
+
+  def initialize(name)
+    @name = name
+    @@roster << add_student
+  end
+
+  def add_student(name, grade)
+    @@roster[:grade] = "#{name}"
+  end
+
+end
