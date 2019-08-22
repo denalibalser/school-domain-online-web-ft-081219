5c71a5abcb0e076175ee5361910a92755fbe4d66
Denali Balser <denali.balser167@gmail.com>
2019-08-22 14:22:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,11 +6,9 @@ class School
     @roster = {}
   end
 
-  def add_student(name, grade)
-
-    @roster[grade] = []
-    @roster[grade] << "name"
-
+  def add_student(student, level)
+    roster[level] ||= []   
+    roster[level] << student
   end
 
 end
